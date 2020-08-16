79a6220c05f761ed2a4686e100907be1bac9d177
Milos Popovic <milos_popovic@outlook.com>
2020-08-16 02:11:01 +0000

    edit lib

--- lib/trying_new_file.rb
+++ lib/trying_new_file.rb
@@ -1,6 +1,17 @@
 require "trying_new_file/version"
 
 module TryingNewFile
-  class Error < StandardError; end
-  # Your code goes here...
+
+require 'net/http'
+require 'json'
+
+# Setting URL
+url = "https://prime.exchangerate-api.com/v5/33abda6f0da266a7c858930b/latest/USD"
+uri = URI(url)
+response = Net::HTTP.get(uri)
+response_obj = JSON.parse(response)
+
+# Getting a rate
+rate = response_obj['conversion_rates']['EUR']
+							
 end
